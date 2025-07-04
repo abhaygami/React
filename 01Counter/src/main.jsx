6de1224837fe9fa9d78570a4f54fab77c816275f
Abhay Gami <163134062+abhaygami@users.noreply.github.com>
2025-07-04 19:20:19 +0530

    Add files via upload

---
+++ 01Counter/src/main.jsx
@@ -0,0 +1,11 @@
+// import {react, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+// import "tailwindcss";
+
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+    <App />
+  // </StrictMode>,
+)
